b4a0f230a3da8e9dcd0b8f9dcc27ca1067ee9549
jlkiri <interalia.dev@gmail.com>
2020-02-21 14:37:18 +0900

    Move back to prismjs

--- src/components/layouts/PostBriefLayout.js
+++ src/components/layouts/PostBriefLayout.js
@@ -6,7 +6,9 @@ const PostBriefLayout = ({ link, title, spoiler, date }) => {
     <div className="pb-10">
       <div className="pb-2">
         <Link to={`${link}`}>
-          <h2 className="text-link leading-9">{title}</h2>
+          <h2 className="text-link font-header font-black leading-9">
+            {title}
+          </h2>
         </Link>
         <span className="font-serif font-bold text-sm">{date}</span>
       </div>
